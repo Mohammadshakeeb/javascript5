7e8b578a92914e6b8b9fb79941c06b8f2ca61bf2
Mohammad Shakeeb <mohammadshakeeb@cedcoss.com>
2022-02-08 12:03:24 +0530

    corrections

--- src/script2.js
+++ src/script2.js
@@ -4,17 +4,25 @@ function funct() {
     var name =document.getElementById("pname").value;
     var price =document.getElementById("pprice").value;
     console.log(id,name,price)
-    // if (id && name && price) {
-        arrProduct.push({ id: id, name: name, price: price });
+    
+if (id && name && price) {
+    if (arrProduct.find((x) => x.id == id)) {
+        alert("enter unique id")
+        return;
+        }
+    
+        arrProduct.push({id:id,name:name,price:price});
         addElement(arrProduct);
-    // } else {
-    //     alert("Please fill all the details");
-    // }
-}
+    
+    }else {
+        alert("Please fill all the details");}
+      
+ }    
+
 function addElement(arr) {
     var table =
         "<table> <tr><th>Product Id</th><th>Product Name</th><th>Product Price</th></tr> ";
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {//printing the full array
         console.log(arr[i]);
         table +=
             "<tr><td>" +
@@ -22,7 +30,8 @@ function addElement(arr) {
             "</td><td>" +
             arr[i].name +
             "</td><td>" +
-            "USD" +arr[i].price +
+            "USD" +arr[i].price +"</td><td>" +
+            '<a href="#" onclick=EditForm(arr[i].id)>Edit</a>' +
             "</td></tr>";
     }
     document.getElementById("table").innerHTML = table + "</table>";
